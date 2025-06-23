9704330588358a2bef6811fc62767146de6da9f8
Harg <harg2001@gmail.com>
2025-06-23 13:27:17 +0200

    Update Footer
    + fix missing font.

---
+++ footer.js
@@ -0,0 +1,6 @@
+// Charger et insérer le header dynamiquement
+	fetch('footer.html')
+	  .then(response => response.text())
+	  .then(data => {
+		document.getElementById('footer-placeholder').innerHTML = data;
+	  });

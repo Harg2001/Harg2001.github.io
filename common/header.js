e04040a6e67580cec520b307b8cf9652b6569539
Harg <harg2001@gmail.com>
2025-07-08 09:37:29 +0200

    Fix issues.

--- common/header.js
+++ common/header.js
@@ -1,6 +1,13 @@
 // Charger et insérer le header dynamiquement
-	fetch('https://harg2001.github.io/common/header.html')
-	  .then(response => response.text())
-	  .then(data => {
-		document.getElementById('header-placeholder').innerHTML = data;
-	  });
+fetch('https://harg2001.github.io/common/header.html')
+  .then(response => response.text())
+  .then(data => {
+	document.getElementById('header-placeholder').innerHTML = data;
+  });
+
+var faces = Array("( ͡° ͜ʖ ͡°)", "¯\_(ツ)_/¯", "ヽ༼ຈل͜ຈ༽ﾉ", "ಠ_ಠ", "¯\(°_o)/¯", "( ﾟдﾟ)", "இдஇ", "(≧▽≦)", "(´_ゝ`)", "´• ل •`", "ʕ•ᴥ•ʔ", "ᶘᵒᴥᵒᶅ", "[̲̅$̲̅(̲̅ιοο̲̅)̲̅$̲̅]", "/̵͇/'̿'̿ ̿ ̿̿ ̿̿ ̿ ̿̿ ̿̿", "(▀̿̿Ĺ̯̿▀̿ ̿)", "凸-_-凸", "(ㆁωㆁ*)", "(•ө•)♡", "(/◕ヮ◕)/", "(^_^.)", 
+"(◉ω◉)", "^오^", "(*´∀｀)", "•̀.̫•́✧", "(๑´ڡ`๑)", "(・∀・)", "༼ ºلº ༽", "ლ(´ڡ`ლ)", "(･ิω･ิ)", "ヽ(^o^)丿", "(*^_^*)", "٩(♡ε♡ )۶", "(๑•̀ㅂ•́)ﻭ✧", "(๑˃̵ᴗ˂̵)ﻭ", "(^o^)", "(✿◠‿◠)", "(＾ｕ＾)", "(-_-;)", "^_^;", "(^o^;", "(ーー;)", "('・ω・')", 
+"(；´Д｀)", "(；･`д･´)", "(´-﹏-`；)", "m(_ _;)m", "( ﾟдﾟ)", "(゜o゜)", "＿|￣|○", "(T_T)", "(｡ŏ﹏ŏ)", "( ･ั﹏･ั)", "(¯―¯٥)", "(｡>﹏<｡)", "(ಥ﹏ಥ)", "༼ಢ_ಢ༽", "(๑´•.̫ • `๑)", "(-̩̩-̩̩͡_-̩̩-̩̩͡)", "(;´༎ຶД༎ຶ`)", "'ㅂ'", "'ㅅ'", "-ㅅ-", "(︶^︶)", "→_→", 
+"(・へ・)", "(~_~メ)", "(ノಠ益ಠ)", "Ƹ̵̡Ӝ̵̨̄Ʒ", "囧", "◕‿◕", "ó_ò", "♪♫*•♪");
+var pickone = faces[Math.floor(Math.random()*faces.length)];
+document.getElementById('face').innerText = pickone;

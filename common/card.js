77d3df188fd2db0807afc8572e45b7186abff580
Harg <harg2001@gmail.com>
2025-07-09 18:13:14 +0200

    Fix issues.

--- common/card.js
+++ common/card.js
@@ -31,19 +31,21 @@ const observer = new MutationObserver((mutationsList, observer) => {
 	  const centerX = rect.width / 2;
 	  const centerY = rect.height / 2;
 
-	  const rotateX = -(y - centerY);
-	  const rotateY = (x - centerX);
+	  var rotateX = -(y - centerY);
+	  var rotateY = (x - centerX);
+	  rotateX = Math.min(-60, Math.max(60, Math.round(boxShadowColor))); // clamp
+	  rotateY = Math.min(-60, Math.max(60, Math.round(boxShadowColor))); // clamp
 	  
 	  card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(2)`;
 	  
-	  // Supposons que rotateX varie entre -30 (vers le haut) et +30 (vers le bas)
-	  const brightness = 1 + rotateX / 90; // varie entre 0.66 et 1.33
+	  // Supposons que rotateX varie entre -50 (vers le haut) et +50 (vers le bas)
+	  const brightness = 1 + rotateX / 150; // varie entre 0.66 et 1.33
 	  card.style.filter = `brightness(${brightness})`;
 
 	  const boxShadowOffsetX = 0
-	  const boxShadowOffsetY = 15* (1 + rotateX / 90)
-	  const boxShadowBlurRadius = 15* (1 + rotateX / 90)
-	  const boxShadowColor = 125* (1 + rotateX / 30) // varie entre 0 et 2
+	  const boxShadowOffsetY = 15* (1 + rotateX / 150)
+	  const boxShadowBlurRadius = 15* (1 + rotateX / 150)
+	  var boxShadowColor = 125* (1 + rotateX / 50) // varie entre 0 et 2
 	  boxShadowColor = Math.min(255, Math.max(0, Math.round(boxShadowColor))); // clamp 0-255
 	  
 	  card.style.boxShadow = `${shadowOffsetX}px ${shadowOffsetY}px ${shadowBlur}px rgba(${shadowColorValue}, ${shadowColorValue}, ${shadowColorValue}, 0.3)`;
@@ -53,7 +55,7 @@ const observer = new MutationObserver((mutationsList, observer) => {
 	card.addEventListener('mouseleave', () => {
 	  card.style.transform = `rotateX(0deg) rotateY(0deg)`;
 	  card.style.filter = ``;
-	  card.style.boxShadow = '0 10px 10px rgba(0,0,0,0.3)';
+	  card.style.boxShadow = '0px 10px 10px rgba(0,0,0,0.3)';
 	});
 
     observer.disconnect();
